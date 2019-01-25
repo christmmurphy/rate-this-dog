e1d907450909e22cd140e758982ae50ce7ab172d
christmmurphy <chris.murphy@intercom.io>
2019-01-25 02:48:19 -0500

    removed emojis that were causing render issues when loaded in messenger

--- app.rb
+++ app.rb
@@ -1,37 +1,25 @@
 require 'json'
 require 'sinatra'
 require 'net/http'
+require 'intercom'
 
-endpoint = "https://949b4bab.ngrok.io"
-
+# ~~~~~~ FUNCTIONS USED BY YOUR APP ~~~~~~ #
+# This function makes a call to an external service and parses the response for an image URL.
 def getDoggo
   url = "https://dog.ceo/api/breeds/image/random"
   uri = URI(url)
   response = Net::HTTP.get(uri)
   dogUrl = JSON.parse(response)
-  puts "hey here's the packet:"+dogUrl["message"]
   output = dogUrl["message"]
 end
 
-get '/' do
-  "Bark bark bark"
-end
+intercom = Intercom::Client.new(token: 'dG9rOjc0YjliN2QyXzcwZDVfNDg4Yl84YjQ5XzE3OWNhMTAwNWUwOToxOjA=')
 
 
-post '/submit' do
-  content_type 'application/json'
-  doggo = getDoggo
-  puts doggo
-  puts "=========================== ~~~~~~~~~~~~  POST SUBMIT  ~~~~~~~~~~~~ ==========================="
-  button = "{\"canvas\":{\"content\":{\"components\":[{\"id\":\"dog\",\"type\":\"image\",\"url\":\"#{doggo}\",\"align\":\"left\",\"width\":340,\"height\":240,\"rounded\":false},{\"id\":\"4a3a1733e96442b0fcc38d2c4f2c\",\"type\":\"single-select\",\"label\":\ null,\"value\":\"3e0820c0e0af9cb653dbf1ae2752\",\"save_state\":\"unsaved\",\"options\":[{\"id\":\"3e0820c0e0af9cb653dbf1ae2752\",\"type\":\"option\",\"text\":\"😁 Good Boy\"},{\"id\":\"a2ae157c6b9878b363aee397590a\",\"type\":\"option\",\"text\":\"😍 Great Boy\"}],\"action\":{\"type\":\"submit\"}}]},\"stored_data\":{}}}"
-  button.to_json
-  puts button
-  button
-end
-
-# Endpoint that's hit when the messenger app is loaded in the messenger.
+# ~~~~~~ ENDPOINTS USED BY INTERCOM ~~~~~~ #
+# INITIALIZE FLOW WEBHOOK URL
+# Endpoint that's hit when your app is loaded in the messenger / home screen
 post'/' do
-  puts "=========================== ~~~~~~~~~~~~  GET  ~~~~~~~~~~~~ ==========================="
   content_type 'application/json'
   puts response.inspect
   response = '{
@@ -41,7 +29,7 @@ post'/' do
               {
                "id": "rate-this-dog-header",
                "type": "text",
-               "text": "Rate This Dog",
+               "text": "Rate This Dog [Staging]",
                "style": "header",
                "align": "center",
                "bottom_margin": false
@@ -71,11 +59,48 @@ post'/' do
         "stored_data": {}
       }
     }'
-  response.to_json
   response
 end
 
-post '/goodboy' do
-  puts "wags tail 🐶"
-  response = '{"canvas":{"content":{"components":[{"id":"welcome-link","type":"image","url":"https://downloads.intercomcdn.com/i/o/86160844/0f0f53fbdb1c741e2f7bca83/link.jpg","align":"center","width":"130","height":"130","rounded":true},{"id":"get-started","type":"button","label":"Rate This Dog 😍","style":"primary","action":{"type":"submit","url":null},"bottom_margin":false}]},"stored_data":{}}}'
+# SUBMIT FLOW WEBHOOK URL
+# Sent when an end-user interacts with your app, via a button, link, or text input. This flow can occur multiple times as an end-user interacts with your app.
+post '/submit' do
+  # Ingest webhook
+  request.body.rewind
+  payload_body = request.body.read
+  response = JSON.parse(payload_body)
+
+  # Pull out variables
+  rating = response["input_values"]["rating-value"]
+  image_url = response["current_canvas"]["content"]["components"][1]["url"]
+  user = response["user"]["id"]
+
+  # Output variables to console
+  puts "Rating Recevied: #{rating}"
+  puts "Dog: #{image_url}"
+  puts "User ID: #{user}"
+  puts "=========================== ~~~~~~~~~~~~  RATING SUBMITED  ~~~~~~~~~~~~ ==========================="
+
+  intercom.events.create(
+    event_name: "Rated Dog",
+    created_at: Time.now.to_i,
+    id: user,
+    metadata: {
+      "Rating:" => "#{rating}",
+      "Dog:" => "#{image_url}"
+    }
+  )
+
+  # Pull a new dog url
+  doggo = getDoggo
+
+  # Insert url into new button object
+  button = "{\"canvas\":{\"content\":{\"components\":[{\"id\":\"rate-this-dog-header\",\"type\":\"text\",\"text\":\"Rate This Dog [Staging]\",\"style\":\"header\",\"align\":\"center\",\"bottom_margin\":false},{\"id\":\"dog\",\"type\":\"image\",\"url\":\"#{doggo}\",\"align\":\"left\",\"width\":340,\"height\":240,\"rounded\":false},{\"id\":\"rating-value\",\"type\":\"single-select\",\"label\":\ null,\"value\":\"good\",\"save_state\":\"unsaved\",\"options\":[{\"id\":\"good\",\"type\":\"option\",\"text\":\"Good Boy\"},{\"id\":\"great\",\"type\":\"option\",\"text\":\"Great Boy\"}],\"action\":{\"type\":\"submit\"}}]},\"stored_data\":{}}}"
+
+  # Send the JSON button as a response to the messenger
+  button
+end
+
+get '/' do
+  "Bark bark bark, there is nothing here."
 end
