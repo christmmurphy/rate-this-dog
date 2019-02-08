ebaf10af81aaa7342095902ea47eefaced836cc2
christmmurphy <chris.murphy@intercom.io>
2019-02-07 18:20:35 -0600

    Added a bunch of comments, refactored code, using ENV variables

--- app.rb
+++ app.rb
@@ -34,7 +34,8 @@ post '/submit' do # When a user presses a button in your app, return a card
   votingCard = "{\"canvas\":{\"content\":{\"components\":[{\"id\":\"dog\",\"type\":\"image\",\"url\":\"#{image}\",\"align\":\"left\",\"width\":340,\"height\":240,\"rounded\":false},{\"id\":\"votingSelection\",\"type\":\"single-select\",\"label\":\ null,\"value\":\"3e0820c0e0af9cb653dbf1ae2752\",\"save_state\":\"unsaved\",\"options\":[{\"id\":\"3e0820c0e0af9cb653dbf1ae2752\",\"type\":\"option\",\"text\":\"#{button1}\"},{\"id\":\"a2ae157c6b9878b363aee397590a\",\"type\":\"option\",\"text\":\"#{button2}\"}],\"action\":{\"type\":\"submit\"}}]},\"stored_data\":{}}}"
 
   puts "======================================================"
-  isvote = ["current_canvas"]["content"]["components"]
+  isvote = response_from_messenger["current_canvas"]["content"]["id"]
+  puts isvote
   puts "======================================================"
 
 
